0b0fd4abf386d4741f78bd5b6755a144735ff42e
stefanos <stliakis@gmail.com>
2023-11-09 14:15:03 +0200

    Refactor logging setup to use a console handler in streamback/utils.py instead of setting the global log config

--- streamback/utils.py
+++ streamback/utils.py
@@ -1,12 +1,13 @@
 import logging
 
-logging.basicConfig(
-    format='streamback - %(levelname)s - %(message)s  ',
-    datefmt='%d-%b-%y %H:%M:%S',
-    level=logging.INFO
-)
-
 logger = logging.getLogger("streamback")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+formatter = logging.Formatter('streamback - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 
 def log(level, msg):
     logger.log(level, msg)
