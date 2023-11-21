3284185317db816b752969f31709517506e803d6
stefanos <stliakis@gmail.com>
2023-11-21 14:48:03 +0200

    Add __repr__ method to FeedbackError class in exceptions.py

--- streamback/exceptions.py
+++ streamback/exceptions.py
@@ -32,3 +32,6 @@ class CouldNotFlushToMainStream(Exception):
 class FeedbackError(Exception):
     def __init__(self, message):
         self.message = message
+
+    def __repr__(self):
+        return self.message
