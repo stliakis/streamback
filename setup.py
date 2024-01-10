a89c1bb4860e7d23c58e5a61a40c1bd73f4f476d
stefanos <stliakis@gmail.com>
2024-01-10 13:13:06 +0200

    Update streamback version from 0.0.36 to 0.0.37.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name="streamback",
-    version="0.0.36",
+    version="0.0.37",
     author='Stefanos Liakis',
     author_email='stliakis@gmail.com',
     description="Two way streams for your microservices",
