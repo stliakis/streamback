9ee3fff8a3be537304fea279d236d43e0cc56c17
stefanos <stliakis@gmail.com>
2024-02-04 01:06:23 +0200

    Fixed the router not registering correctly class based consumers

--- streamback/router.py
+++ streamback/router.py
@@ -1,3 +1,5 @@
+import inspect
+
 from .listener import Listener
 
 
@@ -5,14 +7,15 @@ class Router(object):
     listeners = []
 
     def listen(self, topic, retry=None, input=None, concurrency=None):
-        def decorator(func):
-            self.listeners.append(
-                Listener(topic=topic, function=func, retry_strategy=retry, input=input, concurrency=concurrency)
-            )
-
-            def wrapper_func(*args, **kwargs):
-                return func(*args, **kwargs)
-
-            return wrapper_func
+        def decorator(func_or_listener_class):
+            if inspect.isclass(func_or_listener_class) and issubclass(func_or_listener_class, Listener):
+                self.listeners.append(
+                    func_or_listener_class(topic=topic, retry_strategy=retry, input=input, concurrency=concurrency)
+                )
+            else:
+                self.listeners.append(
+                    Listener(topic=topic, function=func_or_listener_class, retry_strategy=retry, input=input,
+                             concurrency=concurrency)
+                )
 
         return decorator
