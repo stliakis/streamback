efaa1f31e74a4579e6ff0fb09ba1feee1d52fec5
stefanos <stliakis@gmail.com>
2024-08-05 11:44:14 +0300

    Increased the package version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name="streamback",
-    version="0.2.22",
+    version="0.3",
     author='Stefanos Liakis',
     author_email='stliakis@gmail.com',
     description="Two way streams for your microservices",
