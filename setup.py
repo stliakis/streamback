63978b748e177bf9967623328e81c873e0ac8081
stefanos <stliakis@gmail.com>
2024-12-12 11:38:34 +0200

    feat: bump version number in setup.py to 0.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ else:
 
 setup(
     name="streamback",
-    version="0.4.6",
+    version="0.5.0",
     author='Stefanos Liakis',
     author_email='stliakis@gmail.com',
     description="Two way streams for your microservices",
